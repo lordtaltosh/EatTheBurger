db98b87f8c1791762b67e8ae67c29aef6585fb83
Jeremy Sexton <lordtaltosh@gmail.com>
2018-10-25 14:04:16 -0500

    Final Push

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Heart Attack Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tofu No Meat Burger Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style BBQ Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Pepper Jack Mushroom Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Fresco Burger', true);
